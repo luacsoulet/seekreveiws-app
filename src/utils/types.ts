7ed286f14a80596063d216bcf6d9552303b2cb4a
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-06-18 00:06:08 +0200

    Create an Movie and Book interface

---
+++ src/utils/types.ts
@@ -0,0 +1,22 @@
+export interface Movie {
+    id: number
+    title: string
+    description: string
+    director: string
+    genre: string
+    cover_image: string
+    avg_rating: number
+    release_date: string,
+    created_at: string,
+}
+export interface Book {
+    id: number
+    title: string
+    description: string
+    author: string
+    genre: string
+    cover_image: string
+    avg_rating: number
+    release_date: string,
+    created_at: string,
+}
