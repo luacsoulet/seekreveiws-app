94e012b9abc90e2de46793fb28ef404b40843559
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-06-20 01:04:38 +0200

    Create Favorites and Seen interface & add is_seen, seen_id properties

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -8,6 +8,7 @@ export interface Movie {
     avg_rating: number
     release_date: string,
     created_at: string,
+    is_seen?: boolean
 }
 export interface Book {
     id: number
@@ -19,6 +20,8 @@ export interface Book {
     avg_rating: number
     publish_date: string,
     created_at: string,
+    is_seen?: boolean
+    seen_id?: number
 }
 
 export interface Comment {
@@ -28,4 +31,26 @@ export interface Comment {
     created_at: string
     book_id?: number
     movie_id?: number
+}
+
+export interface Favorites {
+    favorite_id: number
+    favorite_at: string
+    created_at: string
+    book_id?: number
+    book_title?: string
+    book_cover?: string
+    movie_id?: number
+    movie_title?: string
+    movie_cover?: string
+}
+export interface Seen {
+    seen_id: number
+    seen_at: string
+    book_id?: number
+    book_title?: string
+    book_cover?: string
+    movie_id?: number
+    movie_title?: string
+    movie_cover?: string
 }
